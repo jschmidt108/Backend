3300208478f2d36bf7c28c12a1490092c399c718
Jacqueline Schmidt <jschmidt108@gmail.com>
2022-05-28 13:17:43 -0400

    installed cors

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
 
 const express = require('express');
 const mongoose = require ('mongoose');
-// const cors = require('cors');
+const cors = require('cors');
 const app = express ();
 require('dotenv').config()
 
@@ -37,7 +37,7 @@ mongoose.connect(MONGODB_URI , () => {
 //___________________
 
 app.use(express.json());
-// app.use(cors());
+app.use(cors());
 
 /////////////////////////////////////////////////////////////////////////
 // routes
